1d03eb9c29263c7722ca9cc3e3ebe7439581be9c
Pedro Bini <dev.pedrobini@gmail.com>
2020-07-23 15:10:12 -0300

    Add interfaces

---
+++ package/src/interfaces/index.ts
@@ -0,0 +1,71 @@
+import { ErrorResponse } from 'apollo-link-error';
+
+/**
+ * Defines options for buildSentryErrorLink.
+ */
+export interface buildSentryErrorLinkOptions {
+  /**
+   * Defines if variables are going to be included in the error report.
+   */
+  includeVariables?: boolean;
+
+  /**
+   * Defines if the operation body should be included in the error report.
+   */
+  includeBody?: boolean;
+
+  /**
+   * Defines if the operation response should be included in the error report.
+   */
+  includeResponse?: boolean;
+
+  /**
+   * Filters if the error should be reported to Sentry.
+   */
+  filter: (error: ErrorResponse) => boolean;
+}
+
+export interface getOperationDataOptions {
+  includeVariables: boolean;
+  includeBody: boolean;
+  error: ErrorResponse;
+}
+
+export interface getResponseDataOptions {
+  error: ErrorResponse;
+}
+
+export interface ExceptionExtraInformation {
+  key: string;
+  value: any;
+}
+
+export interface captureSentryExceptionOptions {
+  extras: Array<ExceptionExtraInformation>;
+  error: ErrorResponse;
+  includeVariables: boolean;
+  includeResponse: boolean;
+  includeBody: boolean;
+}
+
+export interface ErrorOperation {
+  /**
+   * JSON data containing variables information.
+   */
+  variables?: string;
+
+  /**
+   * Operation type.
+   */
+  type: string;
+
+  /**
+   * Operation name.
+   */
+  name: string;
+
+  /**
+   * Operation body.
+   */
+  body?: string;
+}
