36997bbb1a0b217efdc85384db9583fc8aa921db
Pedro Bini <dev.pedrobini@gmail.com>
2020-07-23 15:10:16 -0300

    Add exports

---
+++ package/src/index.ts
@@ -0,0 +1,9 @@
+import buildSentryErrorLink from './links/sentryErrorLink';
+import getErrorMessage from './utils/getErrorMessage';
+import getErrorsList from './utils/getErrorsList';
+
+export {
+  buildSentryErrorLink,
+  getErrorMessage,
+  getErrorsList,
+};
